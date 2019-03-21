4cc031cb9fc214a6151c223b263dbe6e75b6f1b5
Richard Zamora <2220171018@estudiantesunibague.edu.co>
2019-03-20 20:35:30 -0500

    iterfaz principal

--- bibliotecaArchivos/interfaz/GUIPrincipal.Designer.cs
+++ bibliotecaArchivos/interfaz/GUIPrincipal.Designer.cs
@@ -28,17 +28,21 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GUIPrincipal));
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.archivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.grabarLibroToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.leerLibroToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.actualizarLibroToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.eliminarLibroToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.volcarDatosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.salirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.ayudaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.acercaDeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.actualizarLibroToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // menuStrip1
@@ -48,7 +52,7 @@
             this.ayudaToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(284, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(299, 24);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -68,21 +72,28 @@
             // grabarLibroToolStripMenuItem
             // 
             this.grabarLibroToolStripMenuItem.Name = "grabarLibroToolStripMenuItem";
-            this.grabarLibroToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.grabarLibroToolStripMenuItem.Size = new System.Drawing.Size(156, 22);
             this.grabarLibroToolStripMenuItem.Text = "Grabar Libro";
             this.grabarLibroToolStripMenuItem.Click += new System.EventHandler(this.grabarLibroToolStripMenuItem_Click);
             // 
             // leerLibroToolStripMenuItem
             // 
             this.leerLibroToolStripMenuItem.Name = "leerLibroToolStripMenuItem";
-            this.leerLibroToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.leerLibroToolStripMenuItem.Size = new System.Drawing.Size(156, 22);
             this.leerLibroToolStripMenuItem.Text = "Leer Libro";
             this.leerLibroToolStripMenuItem.Click += new System.EventHandler(this.leerLibroToolStripMenuItem_Click);
+            // 
+            // actualizarLibroToolStripMenuItem
+            // 
+            this.actualizarLibroToolStripMenuItem.Name = "actualizarLibroToolStripMenuItem";
+            this.actualizarLibroToolStripMenuItem.Size = new System.Drawing.Size(156, 22);
+            this.actualizarLibroToolStripMenuItem.Text = "Actualizar Libro";
+            this.actualizarLibroToolStripMenuItem.Click += new System.EventHandler(this.actualizarLibroToolStripMenuItem_Click);
             // 
             // eliminarLibroToolStripMenuItem
             // 
             this.eliminarLibroToolStripMenuItem.Name = "eliminarLibroToolStripMenuItem";
-            this.eliminarLibroToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.eliminarLibroToolStripMenuItem.Size = new System.Drawing.Size(156, 22);
             this.eliminarLibroToolStripMenuItem.Text = "Eliminar Libro";
             this.eliminarLibroToolStripMenuItem.Click += new System.EventHandler(this.eliminarLibroToolStripMenuItem_Click);
             // 
@@ -96,7 +107,7 @@
             // salirToolStripMenuItem
             // 
             this.salirToolStripMenuItem.Name = "salirToolStripMenuItem";
-            this.salirToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.salirToolStripMenuItem.Size = new System.Drawing.Size(156, 22);
             this.salirToolStripMenuItem.Text = "Salir";
             this.salirToolStripMenuItem.Click += new System.EventHandler(this.salirToolStripMenuItem_Click);
             // 
@@ -111,28 +122,44 @@
             // acercaDeToolStripMenuItem
             // 
             this.acercaDeToolStripMenuItem.Name = "acercaDeToolStripMenuItem";
-            this.acercaDeToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.acercaDeToolStripMenuItem.Size = new System.Drawing.Size(135, 22);
             this.acercaDeToolStripMenuItem.Text = "Acerca de...";
             this.acercaDeToolStripMenuItem.Click += new System.EventHandler(this.acercaDeToolStripMenuItem_Click);
             // 
-            // actualizarLibroToolStripMenuItem
+            // label1
             // 
-            this.actualizarLibroToolStripMenuItem.Name = "actualizarLibroToolStripMenuItem";
-            this.actualizarLibroToolStripMenuItem.Size = new System.Drawing.Size(156, 22);
-            this.actualizarLibroToolStripMenuItem.Text = "Actualizar Libro";
-            this.actualizarLibroToolStripMenuItem.Click += new System.EventHandler(this.actualizarLibroToolStripMenuItem_Click);
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.label1.Location = new System.Drawing.Point(82, 159);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(132, 31);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Biblioteca";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(19, 45);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(260, 100);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
             // 
             // GUIPrincipal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(299, 217);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "GUIPrincipal";
             this.Text = "Biblioteca";
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -150,6 +177,8 @@
         private System.Windows.Forms.ToolStripMenuItem acercaDeToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem volcarDatosToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem actualizarLibroToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
